4b2e6e02d2b10b303c7a1223f54b85ca219d1326
Ali Kirat <91100475+peripateticlearner@users.noreply.github.com>
2023-09-25 14:56:00 -0700

    function powered modal.

--- js/script.js
+++ js/script.js
@@ -2,12 +2,21 @@ var button = document.querySelector(".got-this");
 var modal = document.querySelector(".modal");
 var modalX = document.querySelector(".modal-x");
 
-button.addEventListener("click", function () {
+var closeModal = function () {
+  modal.classList.remove("show-modal");
+  button.innerText = "Who's got this?";
+};
+
+//Bonus Credit
+var openModal = function () {
   modal.classList.add("show-modal");
   button.innerText = "You've got this!!!";
+};
+
+button.addEventListener("click", function () {
+  openModal();
 });
 
 modalX.addEventListener("click", function () {
-  modal.classList.remove("show-modal");
-  button.innerText = "Who's got this?";
+  closeModal();
 });
