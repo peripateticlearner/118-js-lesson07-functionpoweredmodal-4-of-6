28c3f744ccc42eefb9266b6905d25c014a2324e9
Ali Kirat <91100475+peripateticlearner@users.noreply.github.com>
2023-09-26 18:12:23 -0700

    added Escape key function

--- js/script.js
+++ js/script.js
@@ -20,3 +20,13 @@ button.addEventListener("click", function () {
 modalX.addEventListener("click", function () {
   closeModal();
 });
+
+document.addEventListener("keydown", function (e) {
+  //console.log(e.key);
+  if (e.key === "Escape") {
+    if (modal.classList.contains("show-modal")) {
+      modal.classList.remove("show-modal");
+      button.innerText = "Who's got this?";
+    }
+  }
+});
